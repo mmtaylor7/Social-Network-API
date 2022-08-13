f16475ff16480bb69833fbb5dc966314adb59143
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-13 19:11:05 -0400

    getusers/getsingleuser/createuser copy from act 26

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,26 @@
-const { User, Thought } = require('../models')
+const { User, Thought } = require("../models");
 
 module.exports = {
-    // Get all users
-    getUsers(req, res) {
-      User.find()
-        .then((users) => res.json(users))
-        .catch((err) => res.status(500).json(err));
-    },
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+};
