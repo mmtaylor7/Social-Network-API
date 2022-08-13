dcc03db5d1f402126114179a07ed70873c55b88c
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-13 17:36:53 -0400

    removed virtual in schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,30 +1,22 @@
 const { Schema, Types } = require("mongoose");
 
-const reactionSchema = new Schema(
-  {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxLength: 280,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-      get: () => Date.now,
-    },
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
   },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-    id: false,
-  }
-);
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    get: () => Date.now,
+  },
+});
