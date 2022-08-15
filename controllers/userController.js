d1058b4e6a8078147df8cbd8892ba982604afd85
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-14 21:57:57 -0400

    user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,8 +67,20 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  },
-  removeFriend(req, res) {
 
-  }
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: "No user associated with this id!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
