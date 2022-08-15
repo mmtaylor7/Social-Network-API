a386578a39243f28e75e49cd8a1ed7e0437454c9
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-14 20:39:06 -0400

    get all thoughts route in thoughcontroller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,10 @@
 const { Thought, User } = require("../models");
+
+module.exports = {
+  // get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+};
