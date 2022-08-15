d110a705dd9b1acb51cf855803c48dc2aaf1128d
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-14 21:17:11 -0400

    delete thought (thought controller)

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -59,4 +59,24 @@ module.exports = {
         res.status(500).json(err);
       });
   },
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this id!" })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pull: { thoughts: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "Thought created but no user with this id!",
+            })
+          : res.json({ message: "Thought successfully deleted!" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
